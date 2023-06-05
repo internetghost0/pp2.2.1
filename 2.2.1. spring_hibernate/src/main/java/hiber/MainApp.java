855c18e78f710ccaab93db724b8faf0bc20d74db
zer0 <internetghost0@protonmail.com>
2023-06-05 18:29:24 +0000

    done

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -10,25 +11,32 @@ import java.util.List;
 
 public class MainApp {
    public static void main(String[] args) throws SQLException {
-      AnnotationConfigApplicationContext context = 
-            new AnnotationConfigApplicationContext(AppConfig.class);
+      AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      Car car1 = new Car("Toyota", 123);
+      User user1 = new User("John", "Doe", "john.doe@example.com", car1);
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
+      Car car2 = new Car("Honda", 456);
+      User user2 = new User("Jane", "Smith", "jane.smith@example.com", car2);
+
+      Car car3 = new Car("Mistubishi", 789);
+      User user3 = new User("Kyle", "Sherlock", "kyle.sherlock@example.com", car3);
+
+      userService.add(user1);
+      userService.add(user2);
+      userService.add(user3);
+
+      List<User> foundUsers = userService.getUsersByCarModelAndSeries("Toyota", 123);
+      for (User user : foundUsers) {
+         System.out.println(user);
       }
 
+      List<User> allUsers = userService.listUsers();
+      for (User user : allUsers) {
+         System.out.println(user);
+      }
       context.close();
    }
 }
