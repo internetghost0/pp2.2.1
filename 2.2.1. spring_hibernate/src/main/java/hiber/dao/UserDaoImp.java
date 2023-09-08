0ee810bdf3a9e6823bf2930adcde5089b1a1ff05
zer0 <internetghost0@protonmail.com>
2023-09-08 16:13:25 +0200

    add `JOIN FETCH` to hql-queries for optimization

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -13,28 +13,29 @@ import hiber.model.User;
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
-   @Override
-   public List<User> listUsers() {
-      TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User", User.class);
-      return query.getResultList();
-   }
+    @Override
+    public List<User> listUsers() {
+        String hql = "SELECT DISTINCT u FROM User u JOIN FETCH u.car";
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery(hql, User.class);
+        return query.getResultList();
+    }
 
-   @Override
-   public List<User> getUserByCarModelAndSeries(String model, int series) {
-      String hql = "FROM User u WHERE u.car.model = :model AND u.car.series = :series";
-      return sessionFactory.getCurrentSession()
-            .createQuery(hql, User.class)
-            .setParameter("model", model)
-            .setParameter("series", series)
-            .list();
-   }
+    @Override
+    public User getUserByCarModelAndSeries(String model, int series) {
+        String hql = "SELECT u FROM User u JOIN FETCH u.car c WHERE c.model = :model AND c.series = :series";
+        return sessionFactory.getCurrentSession()
+                .createQuery(hql, User.class)
+                .setParameter("model", model)
+                .setParameter("series", series)
+                .list().get(0); // or .getSingleResult(); but you get errors if db has same model+series cars
+    }
 
 }
